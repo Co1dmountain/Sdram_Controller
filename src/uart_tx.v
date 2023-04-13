35e46bf5820d036885235c59a7926256667c91de
co1dmountain <co1dmountain@xq.com>
2023-04-13 10:26:37 +0800

    add uart_tx.v

---
+++ src/uart_tx.v
@@ -0,0 +1,33 @@
+module uart_tx(
+		//system signals
+		input					sclk				,
+		input					s_rst_n				,
+		//tx_flag
+		input					tx_flag				,
+		//UART Interface
+		output					rs232_tx			,
+		//others
+		output	reg		[7:0]	tx_data				,
+
+);
+
+	//define parameter and internal signals
+	
+	//main code
+	
+	
+
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+endmodule
