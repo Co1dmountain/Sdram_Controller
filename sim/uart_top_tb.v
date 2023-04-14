e1df563eddfc7cebe5e29cdca1c40a21e2cb904c
co1dmountain <co1dmountain@xq.com>
2023-04-14 10:06:01 +0800

    realize uart board verification -> DE2 & zynq board

--- sim/uart_top_tb.v
+++ sim/uart_top_tb.v
@@ -3,7 +3,7 @@
 module uart_top_tb();
 	
 	reg		sys_clk			;
-	reg		sys_rst_n		;
+	//reg		sys_rst_n		;
 	reg		rs232_rx		;
 	
 	wire	rs232_tx		;
@@ -13,10 +13,10 @@ module uart_top_tb();
 	// initial clk, rst, input signals
 	initial begin
 		sys_clk			= 1'b1;
-		sys_rst_n		= 1'b0;
+		//sys_rst_n		= 1'b0;
 		rs232_rx		= 1'b1;
-		#20 
-		sys_rst_n		= 1'b1;
+		//#20 
+		//sys_rst_n		= 1'b1;
 	end
 	
 	always #10 sys_clk			<= ~sys_clk;
@@ -61,7 +61,7 @@ module uart_top_tb();
 	//// inst ////
 	uart_top		U_uart_top(
 			.sys_clk		(sys_clk)		,
-			.sys_rst_n		(sys_rst_n)		,
+			//.sys_rst_n		(sys_rst_n)		,
 			.rs232_rx		(rs232_rx)		,
 			.rs232_tx		(rs232_tx)		
 	);
